a2c7f5d713b6e51f1b6db2aa79f41797616941e9
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-08-04 19:20:48 +0530

    added tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,47 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import "./App.css";
+import Appbar from "./components/Appbar";
+import { Button } from "./components/ui/button";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import Sell from "./pages/Sell";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        OLX clone
-      </div>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WithAppbar element={<Home />} />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route 
+        path="/sell" 
+        element={<WithAppbar element={<Sell />} />}
+        />
+        {/* <Route
+          path="/dashboard"
+          element={<WithAppbar element={<Dashboard />} />}
+        /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+const WithAppbar = ({ element }) => {
+  const location = useLocation();
+
+  const isPublicRoute =
+    location.pathname === "/signup" ||
+    location.pathname === "/login";
+
+  return isPublicRoute ? (
+    element
+  ) : (
+    <>
+      <Appbar />
+      {element}
+    </>
+  );
+};
+
+export default App;
