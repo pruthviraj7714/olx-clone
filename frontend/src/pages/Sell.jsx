61bd9b12632eb33914da95f9515f497d44939d4b
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-08-05 08:41:51 +0530

    added form to sell product

--- frontend/src/pages/Sell.jsx
+++ frontend/src/pages/Sell.jsx
@@ -15,6 +15,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { BACKEND_URL } from "@/config/config";
+import { useNavigate } from "react-router-dom";
 
 const categories = [
   { name: "Electronics", value: "electronics" },
@@ -30,7 +31,7 @@ const Sell = () => {
   const [category, setCategory] = useState("");
   const [image, setImage] = useState(null);
   const [imageUrl, setImageUrl] = useState("");
-
+  const naviagte = useNavigate();
   const { toast } = useToast();
 
   const handleImageChange = (e) => {
@@ -63,7 +64,6 @@ const Sell = () => {
         title: "Image upload failed",
         description: error.response?.data?.message ?? error.message,
       });
-      throw error;
     }
   };
 
@@ -100,113 +100,132 @@ const Sell = () => {
       toast({
         title: res.data.message ?? "Product Successfully Added",
       });
+      naviagte('/')
     } catch (error) {
       toast({
         title: error.response?.data?.message ?? error.message,
+        variant: "destructive"
       });
     }
   };
 
   return (
-    <div className="max-w-md mx-auto p-4 shadow-lg rounded-lg">
-      <h2 className="text-2xl font-bold mb-4">Add Product</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="grid gap-4">
-          <div>
-            <Label htmlFor="productName" className="text-gray-700">
-              Product Name
-            </Label>
-            <Input
-              id="productName"
-              value={productName}
-              onChange={(e) => setProductName(e.target.value)}
-              placeholder="Enter product name"
-              className="w-full"
-            />
-          </div>
-
-          <div>
-            <Label htmlFor="description" className="text-gray-700">
-              Description
-            </Label>
-            <Textarea
-              id="description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              placeholder="Enter product description"
-              className="w-full"
-            />
-          </div>
-
-          <div>
-            <Label htmlFor="price" className="text-gray-700">
-              Price
-            </Label>
-            <Input
-              id="price"
-              type="number"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-              placeholder="Enter product price"
-              className="w-full"
-            />
-          </div>
-
-          <div>
-            <Label htmlFor="category" className="text-gray-700">
-              Category
-            </Label>
-            <Select onValueChange={(value) => setCategory(value)}>
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="Select a category" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Categories</SelectLabel>
-                  {categories.map((cat) => (
-                    <SelectItem key={cat.value} value={cat.value}>
-                      {cat.name}
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
-
-          <div className="flex flex-col">
+    <div className="flex flex-col justify-center items-center min-h-screen ">
+      <h1 className="text-4xl font-extrabold mb-8">
+        Add Your Product To Sell Here
+      </h1>
+      <div className="max-w-md w-full bg-white p-6 shadow-xl rounded-2xl">
+        <h2 className="text-2xl font-bold text-gray-900 mb-6">Add Product</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-6">
             <div>
-              <Label htmlFor="image" className="text-gray-700">
-                Product Image
+              <Label
+                htmlFor="productName"
+                className="text-gray-700 font-semibold"
+              >
+                Product Name
               </Label>
               <Input
-                id="image"
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-                className="w-full"
+                id="productName"
+                value={productName}
+                onChange={(e) => setProductName(e.target.value)}
+                placeholder="Enter product name"
+                className="w-full mt-2 p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
-            {image && (
-              <div className="mt-2">
-                <img
-                  src={URL.createObjectURL(image)}
-                  alt="Selected"
-                  className="w-full h-auto"
-                />
-                <Button onClick={removeImage} className="mt-2">
-                  Remove Image
-                </Button>
-              </div>
-            )}
-          </div>
 
-          <div className="mt-4">
-            <Button type="submit" className="w-full">
-              Add Product
-            </Button>
+            <div>
+              <Label
+                htmlFor="description"
+                className="text-gray-700 font-semibold"
+              >
+                Description
+              </Label>
+              <Textarea
+                id="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Enter product description"
+                className="w-full mt-2 p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="price" className="text-gray-700 font-semibold">
+                Price
+              </Label>
+              <Input
+                id="price"
+                type="number"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                placeholder="Enter product price"
+                className="w-full mt-2 p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="category" className="text-gray-700 font-semibold">
+                Category
+              </Label>
+              <Select onValueChange={(value) => setCategory(value)}>
+                <SelectTrigger className="w-full mt-2 p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+                  <SelectValue placeholder="Select a category" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Categories</SelectLabel>
+                    {categories.map((cat) => (
+                      <SelectItem key={cat.value} value={cat.value}>
+                        {cat.name}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <div className="flex flex-col">
+              <div>
+                <Label htmlFor="image" className="text-gray-700 font-semibold">
+                  Product Image
+                </Label>
+                <Input
+                  id="image"
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  className="w-full mt-2 p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              {image && (
+                <div className="mt-4">
+                  <img
+                    src={URL.createObjectURL(image)}
+                    alt="Selected"
+                    className="w-full h-auto rounded-md"
+                  />
+                  <Button
+                    onClick={removeImage}
+                    className="mt-2 w-full bg-red-500 hover:bg-red-600 text-white font-semibold py-2 rounded-md"
+                  >
+                    Remove Image
+                  </Button>
+                </div>
+              )}
+            </div>
+
+            <div className="mt-6">
+              <Button
+                type="submit"
+                className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 rounded-md"
+              >
+                Add Product
+              </Button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
