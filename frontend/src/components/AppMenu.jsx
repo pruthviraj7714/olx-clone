726fdaff50727846be52a4cbce3c6a193a451417
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-08-05 22:11:39 +0530

    added shadcn components

--- frontend/src/components/AppMenu.jsx
+++ frontend/src/components/AppMenu.jsx
@@ -76,7 +76,7 @@ export function AppMenu() {
                   toast({
                     title : "You Logged out"
                   })
-                  navigate("/signin");
+                  navigate("/login");
                 }}
               >
                 Logout
